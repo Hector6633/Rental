598196b3a01cc543776d74d6e0aa04d05d094124
Rajeev <rajeevlakshmanan003@gmail.com>
2025-11-10 16:45:37 +0530

    Themes module completed.

--- themes/models.py
+++ themes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Feedback_us(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    service = models.CharField(max_length=50)
+    subject = models.CharField(max_length=20)
+    message = models.TextField()
+    
+    def __str__(self) -> str:
+        return self.name
+    
--- themes/views.py
+++ themes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import Feedback_us
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -12,6 +13,23 @@ def service(request):
 def blog(request):
     return render(request, 'blog.html')
 def contact(request):
+    if request.method == "POST":
+        try:
+            name = request.POST['name']
+            email = request.POST['email']
+            phone = request.POST['phone']
+            service = request.POST['service']
+            subject = request.POST['subject']
+            message = request.POST['message']
+            feedback_data = Feedback_us.objects.create(name=name, email=email, phone=phone, service=service, subject=subject,message=message)
+            feedback_data.save()
+            success_msg = "Successfully Registered"
+            messages.success(request, success_msg)
+            return redirect('contact')
+        except Exception as e:
+            error_msg = 'Server Error'
+            messages.error(request, error_msg)
+            return redirect('contact')
     return render(request, 'contact.html')
 def page_not_found404(request):
     return render(request, '404.html')
