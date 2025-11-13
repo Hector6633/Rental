b388825d765fe0ca95cdbcca633192bbb1254ce0
Rajeev <rajeevlakshmanan003@gmail.com>
2025-11-13 23:15:37 +0530

    Services module completed.

--- services/urls.py
+++ services/urls.py
@@ -3,5 +3,6 @@ from . views import *
 
 urlpatterns = [
     path('service/', service, name='service'),
+    path('premium-rental<int:pk>/', premium_car_rental, name='premium_car_rental'),
     path('premium-rental/', premium_car_reservation, name='premium_car_reservation'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,12 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import *
+from django.contrib import messages
 # Create your views here.
 
 def service(request):
     premium_model = {
         'cars': Premium_Car_Model.objects.all()
     }
+    if request.method == 'POST':
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            purpose = request.POST.get('purpose')
+            car_model = request.POST.get('car_model')
+            pickup_location = request.POST.get('pickup_location')
+            dropoff_location = request.POST.get('dropoff_location')
+            pickup_date = request.POST.get('pickup_date')
+            pickup_time = request.POST.get('pickup_time')
+            dropoff_date = request.POST.get('dropoff_date')
+            dropoff_time = request.POST.get('dropoff_time')
+            reservation_data = Car_Reservation.objects.create(name=name, email=email, number=phone, purpose=purpose, car_model=car_model,
+                                pickup_location=pickup_location, dropoff_location=dropoff_location, pickup_date=pickup_date, pickup_time=pickup_time, dropoff_date=dropoff_date, dropoff_time=dropoff_time)
+            success_msg = 'Successfully Booked'
+            messages.success(request, success_msg)
+            reservation_data.save()
+            return redirect('service')
+        except Exception as e:
+            error_msg = 'Something Wrong'
+            messages.error(request, error_msg)
+            return redirect('service')
     return render(request, 'service.html', premium_model)
 
+def premium_car_rental(request, pk):
+    car_model = {
+        'model': Premium_Car_Model.objects.get(pk=pk)
+    }
+    return render(request, 'premium-rental.html', car_model)
+
 def premium_car_reservation(request):
-    return render(request, 'premium-rental.html')
+    if request.method == 'POST':
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            purpose = request.POST.get('purpose')
+            car_model = request.POST.get('car_model')
+            pickup_location = request.POST.get('pickup_location')
+            dropoff_location = request.POST.get('dropoff_location')
+            pickup_date = request.POST.get('pickup_date')
+            pickup_time = request.POST.get('pickup_time')
+            dropoff_date = request.POST.get('dropoff_date')
+            dropoff_time = request.POST.get('dropoff_time')
+            reservation_data = Premium_Car_Reservation.objects.create(name=name, email=email, number=phone, purpose=purpose, car_model=car_model, pickup_location=pickup_location, dropoff_location=dropoff_location, pickup_date=pickup_date, pickup_time=pickup_time, dropoff_date=dropoff_date, dropoff_time=dropoff_time)
+            success_msg = 'Successfully Booked'
+            messages.success(request, success_msg)
+            reservation_data.save()
+            return render(request, 'premium-rental.html')
+        except Exception as e:
+            error_msg = 'Something Wrong'
+            messages.error(request, error_msg)
+            return render(request, 'premium-rental.html')
+            
